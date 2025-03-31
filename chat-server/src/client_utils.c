f3a8e678e1eea255e06e45eaed1bbcb7cbd08de3
Kalina Cathcart <kcathcart3611@conestogac.on.ca>
2025-03-31 14:31:27 -0400

    Push for Daimon's new branch

--- chat-server/src/client_utils.c
+++ chat-server/src/client_utils.c
@@ -77,16 +77,13 @@ void* clientHandler(void* arg) {
 void broadcastMessage(char *message, int senderIndex) {
     pthread_mutex_lock(&clients_mutex);
 
-    // Get the senders IP address as a string
     char senderIP[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(clients[senderIndex].address.sin_addr), senderIP, INET_ADDRSTRLEN);
 
     printf("Broadcasting message from %s: %s\n", clients[senderIndex].username, message);
 
-    // Send to all connected clients
     for (int i = 0; i < MAX_CLIENTS; i++) {
         if (clients[i].active) {
-            // For the sender, use '>>' for others use '<<'
             char direction = (i == senderIndex) ? '>' : '<';
 
             parcelMessage(clients[i].socket, senderIP, clients[senderIndex].username, message, direction);
@@ -128,7 +125,6 @@ bool registerUsername(int index) {
     char buffer[BUFFER_SIZE];
     int bytesRead;
     
-    // First message from client should be the username
     bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
     if (bytesRead <= 0) {
         printf("Client disconnected before sending username\n");
@@ -136,20 +132,17 @@ bool registerUsername(int index) {
         return false;
     }
     
-    //NULL-terminate the received data
     buffer[bytesRead] = '\0';
     
     if (isUsernameTaken(buffer, index)) {
         printf("Client %d attempted to use taken username: %s\n", index, buffer);
         
-        // Send rejection message to client
         const char *msg = "Sorry, that username is already taken. Please reconnect with a different username.\n";
         send(clientSocket, msg, strlen(msg), 0);
         disconnectClient(index);
         return false;
     }
     
-    // Store the username 
     pthread_mutex_lock(&clients_mutex);
     strncpy(clients[index].username, buffer, MAX_NAME_LENGTH - 1);
     clients[index].username[MAX_NAME_LENGTH - 1] = '\0';
@@ -191,19 +184,15 @@ void processClientMessages(int index) {
     char buffer[BUFFER_SIZE];
     int bytesRead;
     
-    // Main message loop - receive and process messages
     while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0)) > 0) {
-        // NULL-terminate the received data
         buffer[bytesRead] = '\0';
         printf("Received from %s: %s\n", clients[index].username, buffer);
         
-        // Check if client wants to disconnect
         if (strcmp(buffer, ">>bye<<") == 0) {
             printf("Client %s is disconnecting\n", clients[index].username);
             break;
         }
         
-        // Process and broadcast the message
         broadcastMessage(buffer, index);
     }
     
@@ -223,7 +212,6 @@ void processClientMessages(int index) {
 * RETURNS:      void
 */
 void handleClientDisconnect(int index) {
-    // Announce that the user has left
     char announcement[BUFFER_SIZE];
     snprintf(announcement, sizeof(announcement), "User %s has left the chat", clients[index].username);
     broadcastMessage(announcement, index);
@@ -244,10 +232,8 @@ void handleClientDisconnect(int index) {
 * RETURNS:      void
 */
 void disconnectClient(int index) {
-    // Lock the mutex before updating shared data
     pthread_mutex_lock(&clients_mutex);
     
-    // Clean up and mark slot as inactive
     close(clients[index].socket);
     clients[index].active = 0;
     clientCount--;
@@ -274,13 +260,10 @@ void formatAndSendMessage(int receiverSocket, char* senderIP, char* senderName, 
     struct tm *timeinfo;
     char timestamp[10];
 
-    // Get current time
     time(&now);
     timeinfo = localtime(&now);
     strftime(timestamp, sizeof(timestamp), "%H:%M:%S", timeinfo);
 
-    // Format according to requirements:
-    // XXX.XXX.XXX.XXX_[AAAAA]_>>_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa_(HH:MM:SS)
     snprintf(formattedMsg, sizeof(formattedMsg),
             "%-15s [%-5s] %c%c %-40s (%s)\n",
             senderIP, senderName, direction, direction, message, timestamp);
@@ -308,41 +291,33 @@ void parcelMessage(int receiverSocket, char* senderIP, char* senderName, char* m
     int messageLen = strlen(message);
     int pos = 0;
 
-    // Break the message into chunks of CHUNK_SIZE characters
     while (pos < messageLen) {
         char chunk[CHUNK_SIZE + 1];
         int chunkSize = 0;
 
-        // Try to break at a space if possible
         int breakPoint = pos + CHUNK_SIZE;
         
-        // If we're beyond the message length, just use the remaining characters
         if (breakPoint >= messageLen) {
             breakPoint = messageLen;
         } else {
-            // Look for a space to break at
             int j = breakPoint;
             while (j > pos && message[j] != ' ') {
                 j--;
             }
-            // If we found a space, break there
             if (j > pos) {
-                breakPoint = j + 1; // Include the space in the first chunk
+                breakPoint = j + 1; 
             }
         }
 
-        // Copy the chunk
         chunkSize = breakPoint - pos;
         if (chunkSize > CHUNK_SIZE) {
-            chunkSize = CHUNK_SIZE; // Ensure we don't exceed CHUNK_SIZE
+            chunkSize = CHUNK_SIZE;
         }
         strncpy(chunk, message + pos, chunkSize);
         chunk[chunkSize] = '\0';
 
-        // Format and send this chunk
         formatAndSendMessage(receiverSocket, senderIP, senderName, chunk, direction);
 
-        // Move to the next position
         pos += chunkSize;
     }
 }
@@ -361,15 +336,14 @@ int isUsernameTaken(char* username, int currentIndex) {
     pthread_mutex_lock(&clients_mutex);
     
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        // Skip checking our own slot
         if (i == currentIndex) continue;
         
         if (clients[i].active && strcmp(clients[i].username, username) == 0) {
             pthread_mutex_unlock(&clients_mutex);
-            return 1; // Username is taken
+            return 1;
         }
     }
     
     pthread_mutex_unlock(&clients_mutex);
-    return 0; // Username is available
+    return 0;
 }
